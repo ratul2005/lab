c880be75fc69d64e944418d5b5531f994d2dcd8f
MD Abdur Rahim Ratul <148898419+ratul2005@users.noreply.github.com>
2025-11-23 09:53:18 +0600

    bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,10 @@
 arr = [9, 4, 1, 7, 3, 10, 2, 6, 8, 5]
 
+def blbsrt(arr):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-1-i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
-for i in range(len(arr)-1):
-    for j in range(len(arr)-1-i):
-        if arr[j]>arr[j+1]:
-            arr[j],arr[j+1] = arr[j+1],arr[j]
-
-print(*arr)            
+blbsrt(arr)
+print(*arr)
